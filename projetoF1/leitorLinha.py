478084bc2e865fc64767a989e5fdd383056cae5a
Pedro <pedrosgregorio@gmail.com>
2023-03-27 17:08:46 -0300

    codigo

--- projetoF1/leitorLinha.py
+++ projetoF1/leitorLinha.py
@@ -4,7 +4,28 @@
 for i in arquivo:
     print(arquivo) """
 
+import re
+
+regex = re.compile("span")
+
+encontrouTBody = False
+
 arquivo = open('race-result.html', 'r', encoding="utf-8")
 for linha in arquivo:
-    print(linha.split("\n?\r"))
+
+    #Encontrar tbody
+    if not encontrouTBody and linha.find("tbody") != -1:
+        encontrouTBody = True
+        continue
+    if encontrouTBody:
+        if linha.find("tbody") != -1:
+            break
+        
+        #Encontrar nomes e pontos
+        if linha.find('class="hide-for-mobile"') != -1 or linha.find('class="bold"') != -1:
+            print(regex.sub(linha.strip(),""))
+            
+            
+    
+    
 arquivo.close()
