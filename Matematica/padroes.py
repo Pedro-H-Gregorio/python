dfe5705c61686634cec8ab47c1332d42f1491bec
Pedro <pedrosgregorio@gmail.com>
2023-04-17 15:20:42 -0300

    Terminando padrões de terceiro grau

--- Matematica/padroes.py
+++ Matematica/padroes.py
@@ -9,11 +9,16 @@ def segundoGrau(y1,y2,y3):
    a ,b = primeiroGrau(y1-c,(y2-c)/2)
    return [a,b,c]
 
+def terceiroGrau(y1,y2,y3,y4):
+  d = -y4 + 4*y3 - 6*y2 + 4*y1
+  a,b,c = segundoGrau(y1-d,(y2-d)/2, (y3-d)/3)
+  return [a,b,c,d]
+
 
 import sys
 
 lista =  list(map(int,sys.argv[1].split(" ")))
-funcao = None
+funcao = []
 controle = []
 
 if len(controle) == 0:
@@ -29,11 +34,23 @@ if len(controle) == 1:
   saoIguais = False
   for i in range(len(lista)):
       saoIguais = valoresABC[0]*((i+1)**2)+valoresABC[1]*(i+1) + valoresABC[2] == lista[i]
-      if saoIguais:
-        funcao = valoresABC
-      else: controle.append(0)
+  if saoIguais:
+    funcao = valoresABC
+  else: controle.append(0)
+if len(controle) == 2:
+  valoresABCD = terceiroGrau(lista[0], lista[1], lista[2], lista[3])
+  saoIguais = False
+  for i in range(len(lista)):
+    saoIguais = valoresABCD[0]*((i+1)**3) + valoresABCD[1]*((i+1)**2) + valoresABCD[2]*(i+1) + valoresABCD[3] == lista[i]
+  if saoIguais:
+    funcao = valoresABCD
+  else: controle.append(0)
 
 if len(funcao) == 2:
   print(f'f(x)= ({funcao[0]})x + ({funcao[1]})')
 elif len(funcao) == 3:
   print(f'f(x)= ({funcao[0]})x² + ({funcao[1]})x + ({funcao[2]})')
+elif len(funcao) == 4:
+  print(f'f(x)= ({funcao[0]})x³+ ({funcao[1]})x² + ({funcao[2]})x + ({funcao[3]})')
+else: print(controle)
+   
