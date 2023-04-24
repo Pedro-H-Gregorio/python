f2f46b594bb0417f3507550f5d107fe444c96085
Pedro <pedrosgregorio@gmail.com>
2023-04-24 17:01:09 -0300

    Adicionando atividade

--- Beecrowd/1018.py
+++ Beecrowd/1018.py
@@ -5,6 +5,6 @@ notasTotal = [0,0,0,0,0,0,0]
 
 for i in range(len(notas)):
     while numero >= notas[i]:
-        numero -= notas[i]
+        numero -= round(notas[i],2)
         notasTotal[i] += 1
 print(f"{n}\n{notasTotal[0]} nota(s) de R$ 100,00\n{notasTotal[1]} nota(s) de R$ 50,00\n{notasTotal[2]} nota(s) de R$ 20,00\n{notasTotal[3]} nota(s) de R$ 10,00\n{notasTotal[4]} nota(s) de R$ 5,00\n{notasTotal[5]} nota(s) de R$ 2,00\n{notasTotal[6]} nota(s) de R$ 1,00")
