a42129117d0f7f80f790505d8e5a61dc01140b85
Pedro <pedrosgregorio@gmail.com>
2023-05-28 18:47:19 -0300

    Refatorando

--- Atividades_Algoritmo/Lista 11/5_quest.py
+++ Atividades_Algoritmo/Lista 11/5_quest.py
@@ -1,6 +1,6 @@
 from functools import reduce
 def fatorial(number):
-    return reduce(lambda x,y: x*y,sorted(range(1,number+1),reverse=True),1)
+    return reduce(lambda x,y: x*y,range(1,number+1),1)
 
 while True:
     n = int(input())
