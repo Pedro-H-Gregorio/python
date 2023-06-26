07309a83560f1d027b3887813074a03d4af85f14
Pedro <pedrosgregorio@gmail.com>
2023-06-26 15:35:54 -0300

    Realizando exercico de Introdução a computação

---
+++ IntroducaoComputacao/exercicioMemoria.py
@@ -0,0 +1,14 @@
+memoria = ['load 6', 'add 7', 'store 6', 'jump 1', 0, 0, 1,1]
+
+pc = 0
+acumulador = 0
+while True:
+    busca = memoria[pc].split()
+    match busca[0]:
+        case 'load' | 'add':
+            acumulador += memoria[int(busca[1])]
+        case 'store':
+            memoria[int(busca[1])] = acumulador
+        case 'jump':
+            pc = int(busca[1]) - 1
+    pc += 1
